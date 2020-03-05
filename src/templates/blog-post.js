5775bfa0b763b6add9efda843f9d7fc4fe9436cb
lucasluz99 <lucasluzthatsme@gmail.com>
2020-03-05 17:54:24 -0300

    Add template to blog-post

---
+++ src/templates/blog-post.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark;
+  return (
+    <>
+      <h1>{post.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </>
+  );
+}
+
+export const query = graphql`
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`;
