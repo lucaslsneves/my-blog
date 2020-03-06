e747f9bcffdadeef0bbd6b5191d7fa5f606788fc
lucasluz99 <lucasluzthatsme@gmail.com>
2020-03-06 16:17:33 -0300

    Add pagination

---
+++ src/templates/blog-list.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
+import PostItem from '../components/PostItem';
+
+export default function BlogList(props) {
+  const postList = props.data.allMarkdownRemark.edges;
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { date, category, description, tagBackground, title },
+            timeToRead,
+            fields: { slug },
+          },
+        }) => (
+          <PostItem
+            slug={slug}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            category={category}
+            description={description}
+            tagBackground={tagBackground || null}
+          />
+        )
+      )}
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query PostList($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY ")
+            description
+            tagBackground
+            title
+          }
+          timeToRead
+        }
+      }
+    }
+  }
+`;
