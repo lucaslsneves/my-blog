9587cca66a84d465ed4852d38f26769f73ad1cff
lucasluz99 <lucasluzthatsme@gmail.com>
2020-03-06 15:22:13 -0300

    Add posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,11 +24,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
-  // Query for markdown nodes to use in creating pages.
-  // You can query for whatever data you want to create pages for e.g.
-  // products, portfolio items, landing pages, etc.
-  // Variables can be added as the second function parameter
+
   return graphql(
     `
       {
